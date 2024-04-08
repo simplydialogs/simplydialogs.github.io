c23c0ef5409098dd2668bef5cd70772d3b10b67e
davidkonrad <davidkonrad@gmail.com>
2024-04-08 04:37:35 +0200

    added wait, setText, addText sections

--- assets/script.js
+++ assets/script.js
@@ -331,6 +331,39 @@ const Test = (function(window, document, SimplyDialogs) {
 		}
 	}
 
+//wait
+	const wait = function() {
+		gebi('btn-wait-settext').onclick = function() {
+			let counter = 0
+			let interval = undefined
+			const wait = Dlg.wait(shortText)
+			const add = function() {
+				counter ++
+				wait.setText(`text or message changed ${counter} time(s)`)
+				if (counter>5) {
+					clearInterval(interval)
+					wait.close()
+				}
+			}
+			interval = setInterval(add, 1000)
+		}
+		gebi('btn-wait-addtext').onclick = function() {
+			let counter = 0
+			let interval = undefined
+			const wait = Dlg.wait(shortText)
+			const add = function() {
+				counter ++
+				wait.addText(`<br>${counter} line(s) added to message`)
+				if (counter>5) {
+					clearInterval(interval)
+					wait.close()
+				}
+			}
+			interval = setInterval(add, 1000)
+		}
+	}
+
+
 //options
 	const options = function() {
 
@@ -635,6 +668,7 @@ const Test = (function(window, document, SimplyDialogs) {
 	formLayout()
 	enterSubmit()
 	escape()
+	wait()
 	options()
 	es()
 	advancedInputs()
